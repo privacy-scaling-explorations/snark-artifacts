1892ec2552966b4beeda64734befee335378e6ad
sripwoud <me@sripwoud.xyz>
2024-04-25 18:00:36 +0200

    update package.json

--- scripts/lib/sparse-checkout.bash
+++ scripts/lib/sparse-checkout.bash
@@ -5,7 +5,7 @@ maybe_init_sparse_checkout() {
 
   git config core.sparseCheckout true
   git config core.sparseCheckoutCone true
-  git config alias.prf '!git fetch --filter=blob:none "$@" && git rebase FETCH_HEAD'
+  git config alias.pull-rebase-filtered '!git fetch --filter=blob:none "$@" && git rebase FETCH_HEAD'
   git sparse-checkout add scripts
 }
 
