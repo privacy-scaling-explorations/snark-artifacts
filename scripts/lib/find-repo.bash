2b0dd9974f68c71cf66cf237a15f76a2c112eb48
sripwoud <me@sripwoud.xyz>
2024-05-06 09:04:08 +0200

    fix: return emtpy string if no repo found from `find_repo`
    otherwise `set -e` causes `find_repo` to be considered
    having failed and the script stops

--- scripts/lib/find-repo.bash
+++ scripts/lib/find-repo.bash
@@ -1,5 +1,9 @@
 find_repo() {
   local repo_dir
   repo_dir=$(find "$HOME" -type f -name "config" -exec grep -q "$REMOTE_GIT_URL" {} \; -print -quit 2>/dev/null)
-  [[ $repo_dir ]] && dirname "$(dirname "$repo_dir")"
+  if [[ $repo_dir ]];then
+    dirname "$(dirname "$repo_dir")"
+  else
+    echo ""
+  fi
 }
