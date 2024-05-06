bd79bb0432d71dfb36659861206ba5705bb4b7ea
sripwoud <me@sripwoud.xyz>
2024-05-06 09:15:22 +0200

    chore: update prompt

--- scripts/lib/clone.bash
+++ scripts/lib/clone.bash
@@ -1,6 +1,6 @@
 clone_repository() {
   local clone_dir
-  clone_dir=$(read_input "Enter the directory to clone into" "directory")
+  clone_dir=$(read_input "Enter the full path of the directory to clone into" "directory")
 
   git clone --filter=blob:none "$REMOTE_GIT_URL" "$clone_dir" --sparse
 
