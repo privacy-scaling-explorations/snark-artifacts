949c57d1eb0ba3785dc68f1a6c50cf0916592085
sripwoud <me@sripwoud.xyz>
2024-05-17 16:18:39 +0200

    docs: add back tsdoc comments

--- packages/artifacts/src/download/download.node.ts
+++ packages/artifacts/src/download/download.node.ts
@@ -48,6 +48,17 @@ async function maybeDownload(url: string) {
   return outputPath
 }
 
+/**
+ * Downloads SNARK artifacts (`wasm` and `zkey`) files if not already present in OS tmp folder.
+ * @example
+ * ```ts
+ * {
+ *   wasm: "/tmp/@zk-kit/semaphore-artifacts@latest/semaphore-3.wasm",
+ *   zkey: "/tmp/@zk-kit/semaphore-artifacts@latest/semaphore-3.zkey"
+ * }
+ * ```
+ * @returns {@link SnarkArtifacts}
+ */
 export default async function maybeGetSnarkArtifacts(
   ...pars: Parameters<typeof _maybeGetSnarkArtifacts>
 ): Promise<SnarkArtifacts> {
