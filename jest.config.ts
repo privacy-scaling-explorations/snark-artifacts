50d291d8c45ede52bcd8383257a35ad0ab890cf4
sripwoud <me@sripwoud.xyz>
2024-05-24 12:05:32 +0200

    test: use shared jest conf

---
+++ jest.config.ts
@@ -0,0 +1,20 @@
+import { join } from 'node:path'
+import type { JestConfigWithTsJest } from 'ts-jest'
+// import preset from 'ts-jest/'
+
+const projects = ['artifacts'].map((name) => `packages/${name}/test`)
+const config: JestConfigWithTsJest = {
+  preset: 'ts-jest',
+  collectCoverage: true,
+  collectCoverageFrom: ['src/**/*.ts'],
+  coverageDirectory: join(__dirname, '..', 'coverage'),
+  coverageThreshold: {
+    global: { branches: 90, functions: 90, lines: 90, statements: 90 },
+  },
+  moduleDirectories: ['node_modules', '<rootDir>/node_modules', '<rootDir>/src'],
+  moduleFileExtensions: ['js', 'ts'],
+  projects,
+  rootDir: '..',
+}
+
+export default config
--- packages/artifacts/test/jest.config.ts
+++ packages/artifacts/test/jest.config.ts
@@ -1,18 +1,11 @@
-import { join } from 'node:path'
 import type { JestConfigWithTsJest } from 'ts-jest'
-// import preset from 'ts-jest/'
+import _sharedJestConf from '../../../jest.config.ts'
+
+const { collectCoverage, projects, ...sharedJestConf } = _sharedJestConf
 
 const config: JestConfigWithTsJest = {
-  preset: 'ts-jest',
-  collectCoverage: true,
-  collectCoverageFrom: ['src/**/*.ts'],
-  coverageDirectory: join(__dirname, '..', 'coverage'),
-  coverageThreshold: {
-    global: { branches: 90, functions: 90, lines: 90, statements: 90 },
-  },
-  moduleDirectories: ['node_modules'],
-  moduleFileExtensions: ['js', 'ts'],
-  rootDir: '..',
+  ...sharedJestConf,
+  displayName: 'artifacts',
 }
 
 export default config
