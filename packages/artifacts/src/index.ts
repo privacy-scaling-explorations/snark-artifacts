a137eae10249bb0f89be21d3413f0157fd3d96cf
sripwoud <me@sripwoud.xyz>
2024-05-27 11:12:14 +0200

    chore: export `getAvailableVersions`

--- packages/artifacts/src/index.ts
+++ packages/artifacts/src/index.ts
@@ -1,5 +1,6 @@
 import maybeGetSnarkArtifacts from './download/download.node'
 
 export * from './download/types'
+export { getAvailableVersions } from './download/urls'
 export * from './projects'
 export { maybeGetSnarkArtifacts }
