0f8abc661456bb1bb3e88fc295dbfcb5626819a5
sripwoud <me@sripwoud.xyz>
2024-06-21 13:26:44 +0200

    fix recursive call

--- packages/artifacts/src/download/index.node.ts
+++ packages/artifacts/src/download/index.node.ts
@@ -1,7 +1,7 @@
 import { tmpdir } from 'node:os'
 import { maybeDownload } from './download.ts'
-import type { SnarkArtifacts } from './types'
 import _maybeGetSnarkArtifacts from './index.browser.ts'
+import type { SnarkArtifacts } from './types'
 
 const extractEndPath = (url: string) => url.split('pse.dev/')[1]
 
@@ -19,7 +19,7 @@ const extractEndPath = (url: string) => url.split('pse.dev/')[1]
 export default async function maybeGetSnarkArtifacts(
   ...pars: Parameters<typeof _maybeGetSnarkArtifacts>
 ): Promise<SnarkArtifacts> {
-  const urls = await maybeGetSnarkArtifacts(
+  const urls = await _maybeGetSnarkArtifacts(
     ...pars,
   )
 
