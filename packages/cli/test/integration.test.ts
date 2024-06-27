99d8b86f39b83aea1ce40d9d0f919a2a5b9c6e7b
sripwoud <me@sripwoud.xyz>
2024-06-27 16:37:37 +0200

    chore: format

--- packages/cli/test/integration.test.ts
+++ packages/cli/test/integration.test.ts
@@ -1,27 +1,26 @@
 // import { writeFileSync } from 'node:fs'
-import { rm } from "node:fs/promises";
-import { stdout } from "node:process";
-import { Cli } from "../src/cli";
+import { rm } from 'node:fs/promises'
+import { stdout } from 'node:process'
+import { Cli } from '../src/cli'
 
-describe("CLI", () => {
-  let cli: Cli;
-  const consoleSpy = jest.spyOn(console, "log");
-  const run = async (args: string[]) =>
-    cli.run(["node", "snarkli.js", ...args]);
+describe('CLI', () => {
+  let cli: Cli
+  const consoleSpy = jest.spyOn(console, 'log')
+  const run = async (args: string[]) => cli.run(['node', 'snarkli.js', ...args])
 
   beforeAll(() => {
     // avoid polluting stdout/sterr with command results' during tests
-    jest.spyOn(stdout, "write").mockImplementation(() => true);
-  });
+    jest.spyOn(stdout, 'write').mockImplementation(() => true)
+  })
   beforeEach(() => {
-    cli = new Cli();
-  });
+    cli = new Cli()
+  })
   afterEach(() => {
-    consoleSpy.mockClear();
+    consoleSpy.mockClear()
     // consoleSpy.mockRestore()
-  });
+  })
 
-  it("should display the help message", async () => {
+  it('should display the help message', async () => {
     cli.cli.exitOverride().configureOutput({
       writeOut(str) {
         expect(str).toMatchInlineSnapshot(`
@@ -37,44 +36,44 @@ Commands:
   list|l                                         List all projects and their available packages versions
   help [command]                                 display help for command
 "
-`);
+`)
       },
-    });
+    })
 
-    await expect(run(["--help"])).rejects.toMatchInlineSnapshot(
-      "[CommanderError: (outputHelp)]"
-    );
-  });
+    await expect(run(['--help'])).rejects.toMatchInlineSnapshot(
+      '[CommanderError: (outputHelp)]',
+    )
+  })
 
-  describe("download", () => {
-    it("should download artifacts for the specified project", async () => {
-      await run(["download", "poseidon", "-p", "2"]);
-      expect(consoleSpy).toHaveBeenCalledTimes(1);
+  describe('download', () => {
+    it('should download artifacts for the specified project', async () => {
+      await run(['download', 'poseidon', '-p', '2'])
+      expect(consoleSpy).toHaveBeenCalledTimes(1)
       expect(consoleSpy.mock.calls[0]).toMatchInlineSnapshot(`
         [
           "Artifacts downloaded at:
           /tmp/@zk-kit/poseidon-artifacts@latest/poseidon-2.wasm
           /tmp/@zk-kit/poseidon-artifacts@latest/poseidon-2.zkey",
         ]
-      `);
-    }, 30_000);
-  });
+      `)
+    }, 30_000)
+  })
 
-  describe("generate", () => {
-    const circomkitJson = "circomkit.json";
+  describe('generate', () => {
+    const circomkitJson = 'circomkit.json'
 
     afterAll(async () => {
       await rm(circomkitJson).catch(() => {
         /* swallow */
-      });
-    });
+      })
+    })
 
-    it.todo("should generate artifacts");
-  });
+    it.todo('should generate artifacts')
+  })
 
-  describe("list", () => {
-    it("should list all projects and the available versions", async () => {
-      await run(["list"]);
+  describe('list', () => {
+    it('should list all projects and the available versions', async () => {
+      await run(['list'])
       expect(consoleSpy.mock.calls[0]).toMatchInlineSnapshot(`
         [
           "semaphore-identity
@@ -89,7 +88,7 @@ Commands:
           4.0.0-beta.11
         ",
         ]
-      `);
-    });
-  });
-});
+      `)
+    })
+  })
+})
