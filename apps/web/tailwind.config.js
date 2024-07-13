15d6e97e8f3c6ba67d5b88c34115dab234ae28a1
sripwoud <me@sripwoud.xyz>
2024-07-13 22:36:33 +0200

    style: update colors palette

--- apps/web/tailwind.config.js
+++ apps/web/tailwind.config.js
@@ -8,7 +8,7 @@ export default {
     colors: {
       aquamarine: '#a0ecd0ff',
       melon: '#ffa69eff',
-      night: '#121619ff',
+      night: '#1a2023',
       raspberry: '#aa4465ff',
       teal: '#247b7bff',
       white: '#f9f9f9',
