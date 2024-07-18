eb703fe9a22b5f4544ecec95d41539d79482d6c3
cedoor <me@cedoor.dev>
2024-07-18 13:49:36 +0100

    chore: recover commits

--- apps/web/src/constants.ts
+++ apps/web/src/constants.ts
@@ -1,4 +1,4 @@
-export const artifacts = ['wasm', 'zkey']
+export const artifacts = ['wasm', 'zkey', 'json']
 export const owner = 'privacy-scaling-explorations'
 export const repo = 'snark-artifacts'
 export const cdnUrl = 'https://snark-artifacts.pse.dev'
