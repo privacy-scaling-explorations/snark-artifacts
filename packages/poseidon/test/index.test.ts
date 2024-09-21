748b8c10cc46a36af7be9fd9e4ecfdd1f7869f5b
sripwoud <me@sripwoud.xyz>
2024-09-21 15:35:54 +0200

    test: use local test package for poseidon test

--- packages/poseidon/test/index.test.ts
+++ packages/poseidon/test/index.test.ts
@@ -1,5 +1,8 @@
-import { generatePoseidonProof, verifyPoseidonProof } from '@zk-kit/test'
+import { Project } from '@zk-kit/artifacts'
+import { generate } from '@zk-kit/poseidon-proof'
+import { SCOPE, verifyPoseidonProof } from '@zk-kit/test'
 import { getCurveFromName } from 'ffjavascript'
+import { join } from 'node:path'
 
 const INPUTS = Array.from({ length: 16 }, (_, i) => i + 1).map(i => ({
   inputs: Array.from({ length: i }, (_, j) => j + 1),
@@ -19,8 +22,11 @@ describe('poseidon', () => {
 
   it.each(INPUTS)(
     'Should verify a poseidon proof with $numberOfInputs input(s)',
-    async ({ inputs }) => {
-      const proof = await generatePoseidonProof(inputs)
+    async ({ inputs, numberOfInputs }) => {
+      const proof = await generate(inputs, SCOPE, {
+        wasm: join(__dirname, '..', `${Project.POSEIDON}-${numberOfInputs}.wasm`),
+        zkey: join(__dirname, '..', `${Project.POSEIDON}-${numberOfInputs}.zkey`),
+      })
       const result = await verifyPoseidonProof(proof)
       expect(result).toBe(true)
     },
